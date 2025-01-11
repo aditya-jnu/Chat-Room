199142a1af85ddf2379627cabeac6ff4e4542a67
aditya-jnu <121703339+aditya-jnu@users.noreply.github.com>
2025-01-11 18:20:40 +0530

    issue

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -15,15 +15,9 @@ export default function InputBox() {
         }
     };
 
-    const handleCreateRoom = () => {
-        const newRoomCode = Math.random().toString(36).substring(2, 8).toUpperCase(); // Example room code generator
-        console.log(`Creating room with code: ${newRoomCode}`);
-        // Add logic to handle creating a room here
-    };
-
     return (
         <div className='h-screen w-screen bg-black flex flex-col gap-5 justify-center items-center'>
-            <p className='text-white font-bold text-5xl'>Party Karyalay</p>
+            <p className='text-white font-bold text-4xl'>Party Karyalay</p>
             <div className="p-5 border rounded-lg max-w-md">
                 <div className=" mb-5">
                     <input type="text" placeholder="Enter room code" value={roomCode} 
@@ -31,14 +25,10 @@ export default function InputBox() {
                         className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                 </div>
-                <div className='flex justify-between'>
+                <div className='flex justify-center'>
                     <button onClick={handleJoinRoom}
                     className="px-6 py-2 mr-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >Join Room</button>
-            
-                    <button onClick={handleCreateRoom}
-                    className="px-6 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-                    >Create Room</button>
                 </div>
             </div>
         </div>
